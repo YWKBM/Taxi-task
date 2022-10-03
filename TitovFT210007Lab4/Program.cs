d90192d8c44fd1c2dbe6eab75f36dc2c9a16e744
YWKBM <ywkbm1973@gmail.com>
2022-10-03 16:39:54 +0500

    Добавьте файлы проекта.

---
+++ TitovFT210007Lab4/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+
+namespace TitovFT210007Lab4
+{
+    static class ArrayExtensions
+    {
+        public static void Swap(this Array arr, int i, int j)
+        {
+            object temp = arr.GetValue(i);
+            arr.SetValue(arr.GetValue(j), i);
+            arr.SetValue(temp, j); 
+        }
+        public static void BubbleSort(this Array arr, IComparer comparer)
+        {
+            for (int i = arr.Length - 1; i >= 0; i--) 
+            {
+                for (int j = 1; j<= i; j++)
+                {
+                    var element1 = arr.GetValue(j);
+                    var element0 = arr.GetValue(j - 1);
+                    if (comparer.Compare(element1, element0) > 0)
+                    {
+                        arr.Swap(j - 1, j);
+                    }
+                }
+            }
+        }
+
+    }
+     class DistanseRateToMaxComparer : IComparer
+    {
+        public int DistanseRateToMax(Taxi taxi)
+        {
+            return taxi.distRate;
+        }
+        public int Compare(object x, object y)
+        {
+            if (DistanseRateToMax((Taxi)x) > DistanseRateToMax((Taxi)y))
+            {
+                return -1;  
+            }
+            else if (DistanseRateToMax((Taxi)x) < DistanseRateToMax((Taxi)y)){
+                return 1;
+            }
+            else return 0;
+        }
+    }
+    class DistanseToZeroComparer : IComparer
+    {
+        public int DistanseToZero(Employee employee)
+        {
+            return employee.distnace;
+        }
+
+        public int Compare(object x, object y)
+        {
+            if (DistanseToZero((Employee)x) > DistanseToZero((Employee)y))
+            {
+                return 1;
+            }
+
+            else if (DistanseToZero((Employee)x) < DistanseToZero((Employee)y))
+            {
+                return -1;
+            }
+
+            else return 0;
+        }
+    }
+    class Employee
+    {
+        public int distnace;
+
+        public Employee(int distanse)
+        {
+            this.distnace = distanse;
+        }
+    }
+    class Taxi
+    {
+        public int number;
+        public int distRate;
+
+        public Taxi(int number, int distRate)
+        {
+            this.number = number;
+            this.distRate = distRate;
+        }   
+    }
+    internal class Program
+    {  
+        static void Main(string[] args)
+        {
+            int totalSum = 0;
+            
+            DistanseRateToMaxComparer distanseRateToMaxComparer = new DistanseRateToMaxComparer();
+            DistanseToZeroComparer distanseToZeroComparer = new DistanseToZeroComparer();
+
+            Console.WriteLine("Enter the quantity of employes: ");
+            int empsQuantity = int.Parse(Console.ReadLine());
+
+            Employee[] emp = new Employee[empsQuantity];
+            Taxi[] taxi = new Taxi[empsQuantity];
+
+            Console.WriteLine("Enter the employes distance to home: ");
+            for(int i = 0; i < empsQuantity; i++)
+            {
+                Console.WriteLine("Emloye's distance {0}", i + 1);
+                emp[i] = new Employee(int.Parse(Console.ReadLine()));    
+            }
+
+            for (int i = 0; i < empsQuantity; i++)
+            {
+                Console.WriteLine("Enter the number of taxi car: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the rate for distance: ");
+                int rate = int.Parse(Console.ReadLine());
+                taxi[i] = new Taxi(num, rate);
+            }
+
+            taxi.BubbleSort(distanseRateToMaxComparer);
+            emp.BubbleSort(distanseToZeroComparer);
+
+            Console.WriteLine("Taxi number | Price for distanse ");
+
+            for (int i = 0; i < empsQuantity; i++)
+            {
+                totalSum += taxi[i].distRate * emp[i].distnace;
+                Console.WriteLine("{0} {1}", taxi[i].number, taxi[i].distRate * emp[i].distnace);
+            }
+
+            Console.WriteLine("Total sum: {0} rub", totalSum);
+            ToWord plur = new ToWord();
+            Console.WriteLine(plur.DigitToWord(totalSum));
+        }
+    }
+}
---
+++ TitovFT210007Lab4/ToWord.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitovFT210007Lab4
+{
+    public class ToWord
+    {
+
+        static string PluralizeCurrencyWord(int count)
+        {
+            switch (count % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    return " рублей";
+                default:
+                    switch (count % 10)
+                    {
+                        case 1:
+                            return " рубль";
+                        case 2:
+                        case 3:
+                        case 4:
+                            return " рубля";
+                        default:
+                            return " рублей";
+                    }
+            }
+        }
+
+        static string PluralizeThousandPartWord(int thousandPart)
+        {
+            switch (thousandPart % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    return " тысяч";
+                default:
+                    switch (thousandPart % 10)
+                    {
+                        case 1:
+                            return " тысяча";
+                        case 2:
+                        case 3:
+                        case 4:
+                            return " тысячи";
+                        default:
+                            return " тысяч";
+                    }
+            }
+        }
+
+
+        public string DigitToWord(int count)
+        {
+            int[] parts = new int[2];
+
+            parts[0] = count / 1000;
+            parts[1] = count % 1000;
+
+            string result = "";
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (parts[i] != 0)
+                {
+
+                    if (parts[i] / 100 != 0)
+                    {
+                        switch (parts[i] / 100)
+                        {
+                            case 1: result += " сто"; break;
+                            case 2: result += " двести"; break;
+                            case 3: result += " триста"; break;
+                            case 4: result += " четыреста"; break;
+                            case 5: result += " пятьсот"; break;
+                            case 6: result += " шестьсот"; break;
+                            case 7: result += " семьсот"; break;
+                            case 8: result += " восемьсот"; break;
+                            case 9: result += " девятьсот"; break;
+                        }
+                    }
+
+                    if (parts[i] % 100 / 10 != 1)
+                    {
+                        switch (parts[i] % 100 / 10)
+                        {
+                            case 2: result += " двадцать"; break;
+                            case 3: result += " тридцать"; break;
+                            case 4: result += " сорок"; break;
+                            case 5: result += " пятьдесят"; break;
+                            case 6: result += " шестьдесят"; break;
+                            case 7: result += " семьдесят"; break;
+                            case 8: result += " восемьдесят"; break;
+                            case 9: result += " девяносто"; break;
+                        }
+
+                        switch (parts[i] % 10)
+                        {
+                            case 1: if (i == 0) result += " одна"; else result += " один"; break;
+                            case 2: if (i == 0) result += " две"; else result += " два"; break;
+                            case 3: result += " три"; break;
+                            case 4: result += " четыре"; break;
+                            case 5: result += " пять"; break;
+                            case 6: result += " шесть"; break;
+                            case 7: result += " семь"; break;
+                            case 8: result += " восемь"; break;
+                            case 9: result += " девять"; break;
+                        }
+
+                    }
+                    else
+                    {
+                        switch (parts[i] % 100)
+                        {
+                            case 10: result += " десять"; break;
+                            case 11: result += " одиннадцать"; break;
+                            case 12: result += " двенадцать"; break;
+                            case 13: result += " тринадцать"; break;
+                            case 14: result += " четырнадцать"; break;
+                            case 15: result += " пятнадцать"; break;
+                            case 16: result += " шестнадцать"; break;
+                            case 17: result += " семнадцать"; break;
+                            case 18: result += " восемннадцать"; break;
+                            case 19: result += " девятнадцать"; break;
+                        }
+                    }
+                }
+
+                if ((i == 0) && parts[0] != 0)
+                {
+                    result += PluralizeThousandPartWord(parts[0]);
+                }
+                else if (i == 1)
+                {
+                    result += PluralizeCurrencyWord(count);
+                }
+
+            }
+
+            return result.Substring(1, 1).ToUpper() + result.Substring(2, result.Length - 2);
+        }
+    }
+}
